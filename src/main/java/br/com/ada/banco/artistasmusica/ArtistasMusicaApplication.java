b3e50161be7f6058cf4893ee495bef2535396ce3
vagrant <vagrant@vagrant.vm>
2022-09-21 17:40:20 +0000

    primeiro commit

---
+++ src/main/java/br/com/ada/banco/artistasmusica/ArtistasMusicaApplication.java
@@ -0,0 +1,13 @@
+package br.com.ada.banco.artistasmusica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistasMusicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtistasMusicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ada/banco/artistasmusica/ArtistasMusicaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ada.banco.artistasmusica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtistasMusicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
